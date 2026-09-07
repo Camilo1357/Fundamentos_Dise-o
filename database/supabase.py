520cc30418db1b5f130b4c0cb8223597895b7567
Your Name <you@example.com>
2026-09-07 11:18:44 -0500

    b

--- database/supabase.py
+++ database/supabase.py
@@ -9,11 +9,6 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-print("================================")
-print("SUPABASE URL:")
-print(repr(SUPABASE_URL))
-print("================================")
-
 supabase: Client = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
